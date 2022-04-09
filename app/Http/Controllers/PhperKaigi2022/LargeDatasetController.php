0e0badf840a6632cb9b71b42fe63ae8827573845
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-04-10 01:56:59 +0900

    update

--- app/Http/Controllers/PhperKaigi2022/LargeDatasetController.php
+++ app/Http/Controllers/PhperKaigi2022/LargeDatasetController.php
@@ -18,6 +18,7 @@ class LargeDatasetController extends Controller
      */
     public function __invoke(Request $request)
     {
+        //DB::enableQueryLog();
         // 100,000 records
         // $start = microtime(true);
         // $books = Book::all(); //Use Eloquent
@@ -27,6 +28,8 @@ class LargeDatasetController extends Controller
         // dump(microtime(true) - $start);// 0.9sec
         // dump(memory_get_usage() / (1024 * 1024));// 185MB
         // dump(memory_get_peak_usage() / (1024 * 1024));// 192MB
+
+        //dd(DB::getQueryLog());
 
         // $start = microtime(true);
         // $books = DB::table('books')->get(); //Use Query Builder
@@ -44,22 +47,24 @@ class LargeDatasetController extends Controller
         //     foreach ($books as $book){}
         // });
 
-        // $start = microtime(true);
-        // $books = DB::table('books')->orderBy('id')->chunk(100, function ($books){
-        //     foreach ($books as $book){
-        //         // do nothing
-        //     }
-        // });
-        // dump(microtime(true) - $start);// 14.19sec
-        // dump(memory_get_usage() / (1024 * 1024));// 1.2MB
-        // dump(memory_get_peak_usage() / (1024 * 1024));// 1.4MB
-
+        //DB::enableQueryLog();
         $start = microtime(true);
-        foreach (DB::table('books')->cursor() as $books) {
-            // do nothing
-        }
-        dump(microtime(true) - $start);// 0.22sec
+        $books = DB::table('books')->orderBy('id')->chunk(1000, function ($books){
+            foreach ($books as $book){
+                // do nothing
+            }
+        });
+        dump(microtime(true) - $start);// 14.19sec
         dump(memory_get_usage() / (1024 * 1024));// 1.2MB
-        dump(memory_get_peak_usage() / (1024 * 1024));// 9.2MB
+        dump(memory_get_peak_usage() / (1024 * 1024));// 1.4MB
+        //dd(DB::getQueryLog());
+
+        // $start = microtime(true);
+        // foreach (DB::table('books')->cursor() as $books) {
+        //     // do nothing
+        // }
+        // dump(microtime(true) - $start);// 0.22sec
+        // dump(memory_get_usage() / (1024 * 1024));// 1.2MB
+        // dump(memory_get_peak_usage() / (1024 * 1024));// 9.2MB
     }
 }
